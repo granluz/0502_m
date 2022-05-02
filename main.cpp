5a5762f4815b23498a8be27f6a1ae9241abbfc88
granluz <granluz@kakao.com>
2022-05-02 09:26:03 +0900

    int float

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+	/*
 	int A = 1, B = 2;
 
 	cout << A + B << endl;
@@ -10,6 +11,15 @@ int main()
 	cout << A * B << endl;
 	cout << A / B << endl;
 	cout << A % B << endl;
-
+	*/
+	float A = 1.2f, B = 2.1f;
+	
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	//cout << A % B << endl;
+	
 	return 0;
+
 }
