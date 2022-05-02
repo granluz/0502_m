156744ab633141ca09033748112ce6709cdc01df
granluz <granluz@kakao.com>
2022-05-02 09:16:25 +0900

    정수형 변수

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A = 1, B = 2;
+
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B << endl;
+
+	return 0;
+}
